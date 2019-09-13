870549fec5a1fc3b4117d322807e399149b09503
Adam Saher <sadam4@myseneca.ca>
2019-09-13 16:39:22 -0400

    First IOS app

---
+++ Week1/Week1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week1
+//
+//  Created by Adam Saher on 2019-09-13.
+//  Copyright © 2019 Adam Saher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
